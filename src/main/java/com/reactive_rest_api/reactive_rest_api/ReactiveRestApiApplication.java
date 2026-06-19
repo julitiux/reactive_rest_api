fe8f0a011450b02305255f296a91bbb12127aee0
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-18 23:24:02 -0600

    starting new project reactive_rest_api

---
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/ReactiveRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.reactive_rest_api.reactive_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive_rest_api/reactive_rest_api/ReactiveRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive_rest_api.reactive_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
