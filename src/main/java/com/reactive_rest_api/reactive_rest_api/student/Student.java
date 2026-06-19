9fe07777ca92e22c6e89207e4ae943428dc404f8
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-19 15:11:47 -0600

    add tag Table from Springframework source

--- src/main/java/com/reactive_rest_api/reactive_rest_api/student/Student.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/student/Student.java
@@ -1,14 +1,18 @@
 package com.reactive_rest_api.reactive_rest_api.student;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "students")
 public class Student {
 
+  @Id
   private Integer id;
   private String firstName;
   private String lastName;
