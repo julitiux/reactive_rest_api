dc20a09fb884e99d081a10fa1c5a8ab8c3293653
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-19 15:08:50 -0600

    add properties on Student class

--- src/main/java/com/reactive_rest_api/reactive_rest_api/student/Student.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/student/Student.java
@@ -1,4 +1,16 @@
 package com.reactive_rest_api.reactive_rest_api.student;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Student {
+
+  private Integer id;
+  private String firstName;
+  private String lastName;
+  private int age;
 }
