273b5b0a836e449b69c8655d319aed8bfa4f5074
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-20 18:13:36 -0600

    add tags like RestController and RequestMapping

--- src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentController.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentController.java
@@ -1,4 +1,9 @@
 package com.reactive_rest_api.reactive_rest_api.student;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
 public class StudentController {
 }
