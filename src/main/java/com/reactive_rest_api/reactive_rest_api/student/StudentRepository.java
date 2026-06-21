735bf2ba180ecaf405320d71b309ce080b783cb7
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-20 18:04:14 -0600

    add method findAllFirstNameIgnoreCase

--- src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentRepository.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentRepository.java
@@ -1,6 +1,9 @@
 package com.reactive_rest_api.reactive_rest_api.student;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface StudentRepository extends ReactiveCrudRepository<Student, Integer> {
+
+  Flux<Student> findAllFirstNameIgnoreCase(String firstName);
 }
