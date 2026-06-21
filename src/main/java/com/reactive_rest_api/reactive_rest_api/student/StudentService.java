a9aef1a923b1c43de33f98cb12448de1252aa0bb
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-20 18:07:01 -0600

    add stereotype tag Service and inject StudentRepository

--- src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentService.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentService.java
@@ -1,4 +1,13 @@
 package com.reactive_rest_api.reactive_rest_api.student;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+
+  private final StudentRepository studentRepository;
+
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
 }
