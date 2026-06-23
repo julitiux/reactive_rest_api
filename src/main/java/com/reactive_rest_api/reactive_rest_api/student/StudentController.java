8d21a8397315585cdc357548905b0b148ae50526
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-22 22:35:10 -0600

    expose the service save with PostMapping

--- src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentController.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentController.java
@@ -1,7 +1,10 @@
 package com.reactive_rest_api.reactive_rest_api.student;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -11,5 +14,10 @@ public class StudentController {
 
   public StudentController(StudentService studentService) {
     this.studentService = studentService;
+  }
+
+  @PostMapping
+  Mono<Student> save(@RequestBody Student student) {
+    return studentService.save(student);
   }
 }
