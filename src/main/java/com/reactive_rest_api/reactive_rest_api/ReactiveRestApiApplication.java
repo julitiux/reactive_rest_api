73f723abbde4309cde0c72c7763096455adcda1b
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-22 23:00:43 -0600

    implement CommandLineRunner for create instances of Student, almost 100

--- src/main/java/com/reactive_rest_api/reactive_rest_api/ReactiveRestApiApplication.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/ReactiveRestApiApplication.java
@@ -1,13 +1,31 @@
 package com.reactive_rest_api.reactive_rest_api;
 
+import com.reactive_rest_api.reactive_rest_api.student.Student;
+import com.reactive_rest_api.reactive_rest_api.student.StudentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReactiveRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveRestApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReactiveRestApiApplication.class, args);
+  }
 
+  @Bean
+  CommandLineRunner commandLineRunner(StudentService studentService) {
+    return args -> {
+      for (int i = 0; i < 100; i++) {
+        studentService.save(
+          Student.builder()
+            .firstname("firstmame" + i)
+            .lastname("lastname" + i)
+            .age(i)
+            .build()
+        );
+      }
+    };
+  }
 }
