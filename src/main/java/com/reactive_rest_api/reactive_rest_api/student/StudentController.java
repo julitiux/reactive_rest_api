fb4cd8c86a7c1cdc5f3cbdbd47b5612049e95a47
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-22 23:38:41 -0600

    especifing of MediaType and add the delayElements

--- src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentController.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentController.java
@@ -1,9 +1,12 @@
 package com.reactive_rest_api.reactive_rest_api.student;
 
 import lombok.Getter;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -20,9 +23,10 @@ public class StudentController {
     return studentService.save(student);
   }
 
-  @GetMapping
+  @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   Flux<Student> findAll(){
-    return studentService.findAll();
+    return studentService.findAll()
+      .delayElements(Duration.ofSeconds(1));
   }
 
   @GetMapping("/{id}")
