880630efef4620b70b8645835cfb30aed431daf5
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-22 22:36:30 -0600

    expose the service findAll with GetMapping

--- src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentController.java
+++ src/main/java/com/reactive_rest_api/reactive_rest_api/student/StudentController.java
@@ -1,9 +1,8 @@
 package com.reactive_rest_api.reactive_rest_api.student;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -19,5 +18,10 @@ public class StudentController {
   @PostMapping
   Mono<Student> save(@RequestBody Student student) {
     return studentService.save(student);
+  }
+
+  @GetMapping
+  Flux<Student> findAll(){
+    return studentService.findAll();
   }
 }
